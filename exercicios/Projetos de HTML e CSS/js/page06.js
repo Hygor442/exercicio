64ff2854f04bf657376b1d9af2d136aea40df6f8
Hygor442 <hygorsix442@gmail.com>
2022-11-09 15:19:35 -0300

    atualização

--- exercicios/Projetos de HTML e CSS/js/page06.js
+++ exercicios/Projetos de HTML e CSS/js/page06.js
@@ -1,3 +1,26 @@
+//scrool da pag
+
+function scrollTo(element){
+    document.querySelector(element).scrollIntoView({behavior:"smooth"});
+}
+
+document.querySelector("#home").addEventListener("click", function(e){
+    e.preventDefault();
+    scrollTo("#inicio");
+});
+document.querySelector("#sobre").addEventListener("click", function(e){
+    e.preventDefault();
+    scrollTo("#diferencial");
+});
+document.querySelector("#alunos").addEventListener("click", function(e){
+    e.preventDefault();
+    scrollTo("#card-aluno");
+});
+document.querySelector("#local").addEventListener("click", function(e){
+    e.preventDefault();
+    scrollTo("#localizacao");
+});
+
 //Slide
 let count = 1;
 document.getElementById("radio1").checked = true;
